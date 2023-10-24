0f83f89e99aab7f69961884561c41bfc2252935b
MaverickTheDeveloper <maverick.the.developer@gmail.com>
2023-10-24 14:10:22 +0900

    complete list page and paging

--- src/app/bbs/list/page.tsx
+++ src/app/bbs/list/page.tsx
@@ -1,14 +1,97 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import styles from './page.module.css'
+import { useSearchParams } from 'next/navigation'
+import PagingBar from '@/components/PagingBar'
 
+type TLine = {
+  id: string
+  title: string
+  author: string
+  date: string
+}
+
+type TDTO = {
+  totalCount: number
+  list: {
+    id: string
+    title: string
+    author: string
+    content: string
+    created_at: string
+    updated_at: string
+  }[]
+}
 type Props = {}
 
 export default function ListPage({}: Props) {
-  const aaa: string = `${styles.pageTitle} ${styles.largeFont}`
+  const searchParams = useSearchParams()
+  const itemsPerPage = 10
+  const currentPage = parseInt(searchParams.get('page') || '1', 10)
+  const [totalCount, setTotalCount] = useState<number>(0)
+  const [list, setList] = useState<TLine[] | null>(null)
+  const noStart = totalCount - ((currentPage - 1) * itemsPerPage)
+
+  async function fetchData(page: number = 1) {
+    let url = 'http://bbsapi.mavericksoft.xyz/bbsapi'
+    let paramsArray: string[] = []
+    if  (page > 1) {
+      paramsArray.push(`p=${page}`)
+    }
+    paramsArray.push(`n=${itemsPerPage}`)
+    const paramsStr = paramsArray.join('&')
+    if (paramsStr.length > 0) {
+      url += '?' + paramsStr
+    }
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'Application/json',
+      },
+    })
+    if (response.ok) {
+      const data: TDTO = await response.json()
+      setTotalCount(data.totalCount)
+      const titleList: TLine[] = data.list.map((item) => {
+        const tempDate = new Date(item.created_at)
+        const newDate = `${tempDate.getFullYear()}-${String(
+          tempDate.getMonth() + 1
+        ).padStart(2, '0')}-${String(tempDate.getDate()).padStart(2, '0')}`
+        return {
+          id: item.id,
+          title: item.title,
+          author: item.author,
+          date: newDate,
+        }
+      })
+      setList(titleList)
+    } else {
+      alert('게시글 목록을 가져오지 못했습니다. 인터넷 연결을 확인하세요')
+    }
+  }
+  useEffect(() => {
+    fetchData(currentPage)
+  }, [currentPage])
   return (
     <div className={styles.ListPage}>
-      <h1>간단 게시판 - 목록</h1>
-      <p className={aaa}>제목</p>
+      <h1>간단 게시판</h1>
+      <div className={styles.listTable}>
+        <div className={styles.listRow}>
+          <p>No.</p>
+          <p>제목</p>
+          <p>글쓴이</p>
+          <p>작성일</p>
+        </div>
+        {list?.map((line, idx) => (
+          <div key={line.id} className={styles.listRow}>
+            <p>{noStart - idx }</p>
+            <p className={styles.leftAlign}>{line.title}</p>
+            <p>{line.author}</p>
+            <p>{line.date}</p>
+          </div>
+        ))}
+      </div>
+      <PagingBar currentPage={currentPage} totalCount={totalCount} itemsPerPage={itemsPerPage}/>
     </div>
   )
 }
---
+++ src/components/PagingBar.tsx
@@ -0,0 +1,67 @@
+'use client'
+import React, { useEffect } from 'react'
+import styles from './PagingBar.module.css'
+import { useRouter } from 'next/navigation'
+
+type Props = {
+  currentPage: number
+  totalCount: number
+  itemsPerPage: number
+}
+
+export default function PagingBar({ currentPage, totalCount, itemsPerPage }: Props) {
+  const router = useRouter()
+  const lastPage = totalCount < 1 ? 1 : Math.ceil(totalCount / itemsPerPage)
+  const startPage = Math.floor((currentPage - 1) / 10) * 10 +  1
+  const endPage = startPage + 9 <= lastPage ? startPage + 9 : lastPage
+
+  const pageArray: number[] = []
+  for (let i = startPage; i <= endPage; i++) {
+    pageArray.push(i)
+  }
+
+  function gotoFirstPage() {
+    router.push('/bbs/list?page=1')
+  }
+  function gotoPrevPage() {
+    router.push(`/bbs/list?page=${currentPage - 1}`)
+  }
+  function gotoThisPage(pageNo: number) {
+    router.push(`/bbs/list?page=${pageNo}`)
+  }
+  function gotoNextPage() {
+    router.push(`/bbs/list?page=${currentPage + 1}`)
+  }
+  function gotoLastPage() {
+    router.push(`/bbs/list?page=${lastPage}`)
+  }
+
+  return (
+    <div className={styles.PagingBar}>
+      <button disabled={currentPage <= 1} onClick={() => gotoFirstPage()}>
+        &lt;&lt;
+      </button>
+      <button disabled={currentPage <= 1} onClick={() => gotoPrevPage()}>
+        &lt;
+      </button>
+      {pageArray.map((pageNo) => {
+        return (
+          <button
+            key={pageNo}
+            className={pageNo === currentPage ? styles.currentPage : ''}
+            disabled={pageNo === currentPage}
+            onClick={() => gotoThisPage(pageNo)}
+          >
+            {pageNo}
+          </button>
+        )
+      })}
+      <button disabled={currentPage >= lastPage} onClick={() => gotoNextPage()}>
+        &gt;
+      </button>
+      <button disabled={currentPage >= lastPage} onClick={() => gotoLastPage()}>
+        &gt;&gt;
+      </button>
+    </div>
+  )
+}
